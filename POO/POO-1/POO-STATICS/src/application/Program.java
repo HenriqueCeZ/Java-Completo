9fe37a991c75f77dba47325b4c56b629607d0049
HenriqueCeZ <henriquecezar.jp@gmail.com>
2022-02-18 14:15:23 -0300

    feat: métodos e atributos statics

---
+++ POO/POO-1/POO-STATICS/src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import util.Calculator;
+import java.util.Scanner;
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter radius: ");
+		double radius = scanner.nextDouble();
+		double c = Calculator.circumference(radius);
+		double v = Calculator.volume(radius);
+		System.out.printf("Circumference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
+
+	}
+
+}
